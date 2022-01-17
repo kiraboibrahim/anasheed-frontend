4269ce59ef4a2063aae187bebb072d93d2845dd8
Kirabo Ibrahim <kiraboibra268@gmail.com>
2022-01-17 06:55:08 -0500

    Added fix for flickering (about page)

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,27 @@
 $(document).ready(function () {
   let nav = $(".navigation");
+  let margin = 0;
   let nav_pos_y = nav.offset().top;
+  let fixed = false;
+  let animationID;
+
+  function fix_nav () {
+    if($(window).scrollTop() >= (nav_pos_y + margin)) {
+      if(!fixed) {
+        nav.addClass("h-sticky");
+        fixed = true;
+      }
+
+    }
+    else {
+      if(fixed) {
+        nav.removeClass("h-sticky");
+        fixed = false;
+      }
+
+    }
+    animationID = requestAnimationFrame(fix_nav);
+  }
   // Open the nav
   $(".m-nav__open-nav").click (function () {
       $(".m-nav__menu").css("left", 0);
@@ -15,13 +36,5 @@ $(document).ready(function () {
     nav_pos_y = nav.offset().top;
   }); // End orientation change
 
-
-  $(window).scroll(function () {
-    if($(this).scrollTop() >= nav_pos_y) {
-      nav.addClass("h-sticky");
-    }
-    else {
-      nav.removeClass("h-sticky");
-    }
-  }); // End scroll
-})
+  requestAnimationFrame(fix_nav);
+}); // end ready
